f600728dc17a9d843ca75e8a41808d6b9bdf1b88
sallymew <sally411@163.com>
2015-08-27 18:41:01 +0800

    首页渲染

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,78 @@
-var React = require('react');
+var React = require('react'),
+    Jquery = require('jquery');
+
+var PlayerList = React.createClass({
+    render: function() {
+        var playerNodes = this.props.data.map(function(player, index){
+            return (
+                <Player avatar={player.avatar} name={player.name} platform={player.platform} players={player.players} slogan={player.slogan} />
+            );
+        });
+        return (
+            <div>
+                <ul id="tabs">
+                    <li className="current">招募榜</li>
+                    <li>活跃榜</li>
+                </ul>
+                <div id="playerList">
+                    <div id="info">正在招募成员的公会，按发布招募时间排名</div>
+                    {playerNodes}
+                </div>
+            </div>
+        )
+    }
+});
+
+var Player  = React.createClass({
+    render: function() {
+        return (
+            <div className="list-item">我是一个玩家</div>
+        )
+    }
+});
 
 var Page = React.createClass({
+    getInitialState: function() {
+        return {num: '28.2万', player: '395.7万', data: []};
+    },
+    componentDidMount: function() {
+        this.getPlayerListFromServer();
+    },
+    getPlayerListFromServer: function() {
+        Jquery.ajax({
+            url: this.props.url,
+                dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
-        return <h1>QQ首页大厅</h1>
+        return (
+            <div id="content">
+                <div id="searchContainer">
+                    <div id="searchContent">
+                        <span id="search">请输入公会ID</span>
+                    </div>
+                </div>
+                <div id="banner"></div>
+                <h3>
+                    已有<em>{this.state.num}</em>个公会入驻，累计<em>{this.state.player}</em>位玩家
+                </h3>
+                <div id="statusImg">
+                    <button id="joinBtn"></button>
+                    <button id="createBtn"></button>
+                </div>
+                <PlayerList data={this.state.data}/>
+            </div>
+        )
     }
 });
 
 React.render(
-    <Page />,
-    document.getElementById('content')
+    <Page url="playerList.json"/>,
+    document.getElementById('container')
 );
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var path = require('path'),
     bodyParser = require('body-parser'),
-    expresss = require('express');
+    expresss = require('express'),
+    fs = require('fs');
 
 var app = expresss();
 app.use('/', expresss.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.get('/playerList.json', function(req, res) {
+    fs.readFile('playerList.json', function(err, data) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    });
+});
 app.listen(8888);
 console.log('Server started: http://localhost:8888/');
